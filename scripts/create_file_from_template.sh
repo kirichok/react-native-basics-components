2c7057c4ff35eb2fd96080230c265838a7257eef
Evgeniy Kirichok <evgeniy.kirichok@gmail.com>
2019-10-18 15:56:24 +0300

    - Updated output info text if not found template

--- scripts/create_file_from_template.sh
+++ scripts/create_file_from_template.sh
@@ -2,4 +2,4 @@ search="{PWD}"
 replace=$(echo $PWD | sed 's_/_\\/_g')
 file=$(pwd)/react-native.config.template.js
 
-test -f $file && sed "s/${search}/${replace}/g" $file > react-native.config.js || echo none
+test -f $file && sed "s/${search}/${replace}/g" $file > react-native.config.js || echo "Not found 'react-native.config.template.js'"
